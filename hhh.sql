4f6fde5a1dd2adc2f3b17c5afe68e765562e1dac
YingJames <jamesy148@usf.edu>
2024-07-19 11:48:40 -0400

    update comments

--- hhh.sql
+++ hhh.sql
@@ -85,32 +85,19 @@ GROUP BY u.UID, u.Username, u.Email
 ORDER BY HardProblemCount DESC;
 
 -- Create functions
-CREATE OR REPLACE FUNCTION update_player_quest_completion()
-RETURNS TRIGGER AS $$
-BEGIN
-    UPDATE PlayerQuests pq
-    SET IsCompleted = (
-        SELECT CASE
-            WHEN COUNT(*) = (SELECT COUNT(*) FROM QuestProblems WHERE QID = pq.QID)
-            THEN TRUE
-            ELSE FALSE
-        END
-        FROM QuestProblems qp
-        JOIN CompletedProblems cp ON qp.PID = cp.PID
-        WHERE qp.QID = pq.QID AND cp.UID = pq.UID
-    )
-    WHERE pq.UID = NEW.UID OR pq.UID = OLD.UID;
-    RETURN NULL;
-END;
-$$ LANGUAGE plpgsql;
+
 
 -- Create triggers
+
+-- Enforces distinct Subclasses --
+-- Used to prevent Admins from being Players
 CREATE OR REPLACE FUNCTION check_admin_player_conflict()
 RETURNS TRIGGER AS $$
 BEGIN
     IF EXISTS (SELECT 1 FROM Players WHERE UID = NEW.UID) THEN
         RAISE EXCEPTION 'User is already a Player';
     END IF;
+    -- ensures new row is passed if no exception
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
@@ -120,6 +107,7 @@ BEFORE INSERT ON Admins
 FOR EACH ROW
 EXECUTE FUNCTION check_admin_player_conflict();
 
+-- Used to prevent Players from being Admins
 CREATE OR REPLACE FUNCTION check_player_admin_conflict()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -134,7 +122,9 @@ CREATE TRIGGER player_admin_conflict_check
 BEFORE INSERT ON Players
 FOR EACH ROW
 EXECUTE FUNCTION check_player_admin_conflict();
+-----------------------------------
 
+-- prevents non-admins from creating quests
 CREATE OR REPLACE FUNCTION check_quest_creator()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -150,6 +140,7 @@ BEFORE INSERT ON Quests
 FOR EACH ROW
 EXECUTE FUNCTION check_quest_creator();
 
+-- prevent problem or topic from being added to ProblemTopics if they do not exist
 CREATE OR REPLACE FUNCTION check_problem_topic()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -168,6 +159,7 @@ BEFORE INSERT ON ProblemTopics
 FOR EACH ROW
 EXECUTE FUNCTION check_problem_topic();
 
+-- prevent quest or problem from being added to QuestProblems if they do not exist
 CREATE OR REPLACE FUNCTION check_quest_problem()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -186,6 +178,7 @@ BEFORE INSERT ON QuestProblems
 FOR EACH ROW
 EXECUTE FUNCTION check_quest_problem();
 
+-- prevent user or problem from being added to CompletedProblems if they do not exist
 CREATE OR REPLACE FUNCTION check_completed_problem()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -204,6 +197,7 @@ BEFORE INSERT ON CompletedProblems
 FOR EACH ROW
 EXECUTE FUNCTION check_completed_problem();
 
+-- prevent user or quest from being added to PlayerQuests if they do not exist
 CREATE OR REPLACE FUNCTION check_player_quest()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -222,6 +216,7 @@ BEFORE INSERT ON PlayerQuests
 FOR EACH ROW
 EXECUTE FUNCTION check_player_quest();
 
+-- replaces the UID of a deleted admin with the default admin UID in Quests
 CREATE OR REPLACE FUNCTION update_quest_admin()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -232,10 +227,34 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+-- in the case that an admin is deleted, we do not want Quests to have NULL as the UID
 CREATE TRIGGER update_quest_admin_trigger
 BEFORE DELETE ON Admins
 FOR EACH ROW
 EXECUTE FUNCTION update_quest_admin();
+
+-- updates the IsCompleted field in PlayerQuests when all problems in a quest are completed
+CREATE OR REPLACE FUNCTION update_player_quest_completion()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE PlayerQuests pq
+    -- subquery returns true or false based on if the count of completed problems for a quest is equal to the count of problems in the quest
+    SET IsCompleted = (
+        SELECT CASE
+            WHEN COUNT(*) = (SELECT COUNT(*) FROM QuestProblems WHERE QID = pq.QID)
+            THEN TRUE
+            ELSE FALSE
+        END
+        FROM QuestProblems qp
+        JOIN CompletedProblems cp ON qp.PID = cp.PID
+        WHERE qp.QID = pq.QID AND cp.UID = pq.UID
+    )
+    -- allows the function to work for both INSERT and DELETE operations
+    WHERE pq.UID = NEW.UID OR pq.UID = OLD.UID;
+    -- does not need to return a value if just modifying the table
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
 
 CREATE TRIGGER update_player_quest_completion_trigger
 AFTER INSERT OR DELETE ON CompletedProblems
@@ -254,7 +273,7 @@ VALUES ('00000000-0000-0000-0000-000000000000');
 GRANT SELECT ON UsersView TO admin_user;
 GRANT INSERT ON Users TO admin_user;
 GRANT SELECT ON Players TO admin_user;
-GRANT SELECT, INSERT, UPDATE, DELETE ON Admins TO admin_user;
+GRANT ALL PRIVILEGES ON Admins TO admin_user;
 
 CREATE USER scout_user WITH PASSWORD 'scout_password';
 GRANT SELECT ON HardestPlayers TO scout_user;
